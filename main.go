e3a1ebc938b3dc49df14cde77f63d0845e9110ec
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2024-08-03 10:07:15 +0900

    Graceful shutdownをサポートする (#3)

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/TechBowl-japan/go-stations/db"
@@ -49,8 +54,49 @@ func realMain() error {
 
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
+	server := &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
 
-	// TODO: サーバーをlistenする
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	defer stop()
+	var wg sync.WaitGroup
+
+	// NOTE: serverの数だけAddする
+	wg.Add(1)
+	go run(ctx, &wg, server)
+	wg.Wait()
 
 	return nil
+}
+
+// run はHTTPサーバに対するGraceful shutdownを提供する。
+//
+// [context.Context] 及び [sync.WaitGroup]を共有する事で複数サーバのGraceful shutdownを同時に制御できる。
+func run(ctx context.Context, wg *sync.WaitGroup, srv *http.Server) {
+	go func() {
+		defer wg.Done()
+
+		<-ctx.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("main: could not gracefully shutdown the server, err =%v\n", err)
+		} else {
+			log.Printf("main: server is completely shutdown\n")
+		}
+	}()
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Printf("main: could not listen on %v, err =%v\n", srv.Addr, err)
+	} else {
+		log.Printf("main: listen port %v is closed\n", srv.Addr)
+	}
 }
